e2c3c563a816fe5ed54188d44bf634935880c11f
Nikita <sonterixo@gmail.com.com>
2017-12-12 14:10:37 +0200

    added new skills

--- js/main.js
+++ js/main.js
@@ -4,13 +4,24 @@ new Vue({
         message: 'Hellow',
         textInput: 'Type text',
         showText: false,
+        htmlText: 'Link: <a href="http://google.com.ua/">Google</a>',
+        counter: 0,
+        x: 0,
+        y: 0,
     },
     methods: {
-        changeText(){
+        changeText: function(){
             this.message = 'Hellow World';
         },
-        changeInput(event){
+        changeInput: function(event){
             this.textInput = event.target.value;
+        },
+        increase: function(){
+            this.counter++;
+        },
+        updateCoordinates: function(event){
+            this.x = event.clientX;
+            this.y = event.clientY;
         }
     }
 });
